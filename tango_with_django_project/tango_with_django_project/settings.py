655b90459c9d8f4434b00d16adb60f3c44cfbca5
Dharmesh Kamdar <dkamdar5@users.noreply.github.com>
2015-02-26 14:21:46 -0600

    Templates and Static Media
    Set up and create templates. Use templates in views.
    Set up Django to send static media, and include images on templates.
    Set up Django's static media server to allow file uploads.

--- tango_with_django_project/tango_with_django_project/settings.py
+++ tango_with_django_project/tango_with_django_project/settings.py
@@ -9,9 +9,27 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 """
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+#PROJECT_PATH = os.path.join(SETTINGS_DIR, os.pardir)
+#PROJECT_PATH = os.path.abspath(PROJECT_PATH)
+
 import os
+from django.conf.global_settings import MEDIA_URL
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
+#print (__file__)
+#print (os.path.dirname(__file__))
+#print (os.path.dirname(os.path.dirname(__file__)))
+
+TEMPLATE_PATH = os.path.join(BASE_DIR, 'templates')
+
+STATIC_PATH = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/' # You may find this is already defined as such.
+STATICFILES_DIRS = (
+    STATIC_PATH,
+)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media') #absolute path to media directory
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
@@ -26,6 +44,15 @@ TEMPLATE_DEBUG = True
 
 ALLOWED_HOSTS = []
 
+
+
+#TEMPLATE_DIRS = ['C:/Users/Dharmesh/Documents/GitHub/Rango/tango_with_django_project/']
+TEMPLATE_DIRS = [
+    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    TEMPLATE_PATH,
+]
 
 # Application definition
 
--- tango_with_django_project/tango_with_django_project/urls.py
+++ tango_with_django_project/tango_with_django_project/urls.py
@@ -1,4 +1,5 @@
-from django.conf.urls import patterns, include, url
+from django.conf import settings
+from django.conf.urls import patterns, include, url, static
 from django.contrib import admin
 
 urlpatterns = patterns('',
@@ -9,3 +10,16 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^rango/', include('rango.urls')), #new tuple
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns(
+        'django.views.static', #handles uploading of media
+        (r'^media/(?P<path>.*)', #change path to ^
+         'serve',
+         {'document_root': settings.MEDIA_ROOT}), #defined in settings.py
+    )
+
+# Deployment step
+# set DEBUG to False in settings.py
+if not settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
