53b9f8e5c1e4ac21863ca7e9251c9de9d896dd4b
Dharmesh Kamdar <dkamdar5@users.noreply.github.com>
2015-02-27 16:33:55 -0600

    Exercise
    Added attributes to Category model. Made migrations. Updated script.
    Updated admin interface.

--- tango_with_django_project/rango/admin.py
+++ tango_with_django_project/rango/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
 from rango.models import Category, Page
 
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'url')
+    
+class CatAdmin(admin.ModelAdmin):
+    list_display = ('name', 'views', 'likes')
+
 #register application models with admin
-admin.site.register(Category)
-admin.site.register(Page)
+admin.site.register(Category, CatAdmin)
+admin.site.register(Page, PageAdmin)
